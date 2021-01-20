58e76b6ea07a055fa3c5bb12ae56f0ca33bde173
lisusmikhail <lisusmikhail@gmail.com>
2021-01-20 21:30:13 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
+    
     render() {
       return (  
         <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
           <table className="table table-bordered">
             <tbody>
             {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
+                this.props.emotions.map((emotion, i)=>{
+                   return <tr key={i}>
+                       <th>{emotion[0]}</th>
+                       <th>{emotion[1]}</th>
+                   </tr>
+                })
             }
             </tbody>
           </table>
--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,33 +1,126 @@
 const express = require('express');
 const app = new express();
-
+const dotenv = require('dotenv');
+dotenv.config();
 app.use(express.static('client'))
+
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const {IamAuthenticator} = require('ibm-watson/auth');
+    return new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    })
+}
+
+
+function understandTextLanguageSentiment(textToAnalyze, res) {
+    let naturalLanguageUnderstanding = getNLUInstance();
+    const analyzeParams = {
+        'features': {
+            'sentiment': {}
+        },
+        'text': textToAnalyze
+    };
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            const labelAnalysisResult=analysisResults.result.sentiment.document.label
+            res.send(`${labelAnalysisResult}`)
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
+}
+
+
+function understandTextLanguageEmotion(textToAnalyze, res) {
+    let naturalLanguageUnderstanding = getNLUInstance();
+    const analyzeParams = {
+        'features': {
+            'emotion': {}
+        },
+        'text': textToAnalyze
+    };
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            const emotionResult = analysisResults.result.emotion.document.emotion
+            console.log(emotionResult, typeof emotionResult)
+            res.send(emotionResult)
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
+}
+
+function understandUrlLanguageEmotion(urlToAnalyze, res) {
+    let naturalLanguageUnderstanding = getNLUInstance();
+    const analyzeParams = {
+        'url': urlToAnalyze,
+        'features': {
+            'emotion': {}
+        }
+    };
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            const emotionResult = analysisResults.result.emotion.document.emotion
+            res.send(emotionResult)
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
+}
+
+function understandUrlLanguageSentiment(urlToAnalyze, res) {
+    let naturalLanguageUnderstanding = getNLUInstance();
+    const analyzeParams = {
+        'url': urlToAnalyze,
+        'features': {
+            'sentiment': {}
+        }
+    };
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            const labelAnalysisResult=analysisResults.result.sentiment.document.label
+            res.send(`${labelAnalysisResult}`)
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
+}
+
 
 const cors_app = require('cors');
 app.use(cors_app());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('index.html');
-  });
-
-app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
 });
 
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+app.get("/url/emotion", (req, res) => {
+    const url = req.query.url;
+    understandUrlLanguageEmotion(url, res)
 });
 
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+app.get("/url/sentiment", (req, res) => {
+    const url = req.query.url;
+    understandUrlLanguageSentiment(url, res)
 });
 
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+app.get("/text/emotion", (req, res) => {
+    let textToEmotion = req.query.text
+    understandTextLanguageEmotion(textToEmotion, res)
+});
+
+app.get("/text/sentiment", (req, res) => {
+    let textToSentiment = req.query.text
+    understandTextLanguageSentiment(textToSentiment, res)
 });
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
-
